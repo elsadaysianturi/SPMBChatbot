68b58dbfeb725e6219d0a995b2c6c44d9bcf056c
Elsaday Sianturi <elsadaysianturi@gmail.com>
2024-06-10 09:20:55 +0700

    Fixing UI

--- public/assets/js/chatbot.js
+++ public/assets/js/chatbot.js
@@ -1,8 +1,8 @@
 var chatbotButton = document.getElementById("chatbot-button");
 var chatbotContainer = document.getElementById("chatbot-container");
 var closeChatbotButton = document.querySelector(".close-chatbot");
-var closeIcon =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
+// var closeIcon =
+//     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';
 
 chatbotButton.addEventListener("mouseenter", function () {
     if (
@@ -21,7 +21,7 @@ chatbotButton.addEventListener("mouseleave", function () {
             .getPropertyValue("display") === "block"
     ) {
         chatbotButton.innerHTML =
-            '<img src="assets/images/chatbot.png" alt="Chatbot Icon">';
+            '<img src="assets/images/icons8-chat-90.png" alt="Chatbot Icon">';
     }
 });
 
@@ -37,7 +37,7 @@ chatbotButton.onclick = function () {
     ) {
         chatbotContainer.style.display = "none";
         chatbotButton.innerHTML =
-            '<img src="assets/images/chatbot.png" alt="Chatbot Icon">';
+            '<img src="assets/images/icons8-chat-90.png" alt="Chatbot Icon">';
     } else {
         chatbotContainer.style.display = "block";
         chatbotButton.innerHTML = closeIcon;
